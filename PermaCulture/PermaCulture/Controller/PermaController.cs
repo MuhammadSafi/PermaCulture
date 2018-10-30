b1376f0c7700521a195cacc6d327703ff0ab682c
muhammad.safics@hotmail.com <muhammad.safics@hotmail.com>
2018-10-30 08:19:50 +0400

    adding solution

---
+++ PermaCulture/PermaCulture/Controller/PermaController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PermaCulture.API.Controller
+{
+
+    public class PermaController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/ping")]
+        public ActionResult Ping()
+        {
+
+            return Ok("test");
+        }
+    }
+}
