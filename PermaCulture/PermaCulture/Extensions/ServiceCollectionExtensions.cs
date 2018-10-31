d59f1e4fffdb109f78138a1d2629501e2e03e201
muhammad.safics@hotmail.com <muhammad.safics@hotmail.com>
2018-10-31 13:00:48 +0400

    add files.

---
+++ PermaCulture/PermaCulture/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PermaCulture.Api.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection ConfigureSwagger(this IServiceCollection services, IConfiguration configuration)
+        {
+        }
+    }
+}
